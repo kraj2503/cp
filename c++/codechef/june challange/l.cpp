f9d0d30cd0e4ee796c137ba72be9096dcb6de45b
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2021-06-19 09:36:43 +0530

    tanmay exam

---
+++ c++/codechef/june challange/l.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int Add(int x, int y, int z)
+{
+    return x + y;
+}
+double Add(double x, double y, double z)
+{
+    return x + y;
+}
+int main()
+{
+    cout << add(5, 6);
+    cout << add(5.5, 6.6);
+
+    return 0;
+}
